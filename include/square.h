eddb49cbbefe8b909d0a548ea88bdd6292b8953f
tifui-alexandru <itifui00@gmail.com>
2020-04-15 22:38:39 +0300

    pt square

--- include/square.h
+++ include/square.h
@@ -11,6 +11,18 @@ public:
     inline std::pair <char, int> chess_notation_pos() const {
         return std::make_pair(y + 1, static_cast<char>(x + 'a'));
     }
+
+    inline bool valid() const {
+        return x >= 0 and y >= 0 and x < 8 and y < 8;
+    }
+
+    Square operator + (const Square &other) const {
+        return Square(x + other.x, y + other.y);
+    }
+
+    Square operator * (const Square &other) const {
+        return Square(x * other.x, y + other.y);
+    }
 };
 
 #endif
--- src/king.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-#include "king.h"
-
-King::King(const bool &col = false, const Square &pos = {-1, -1}) : Piece(col, pos, 0, 5) {}
-
-std::vector <Square> King::get_possible_moves() {
-
-}

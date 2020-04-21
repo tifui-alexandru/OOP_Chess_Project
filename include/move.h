28484aef0244ad61cf69a74c389d09cf6873cdf6
tifui-alexandru <itifui00@gmail.com>
2020-04-21 12:52:42 +0300

    not finished notation/ chess engine added

--- include/move.h
+++ include/move.h
@@ -9,10 +9,11 @@ private:
     Square from, to;
     Board *curr_board, *next_board;
     PieceColour col;
+    static std::unordered_map <PieceType, std::string> AlgebraicPiece;
 
 public:
     Move(const Square &from, const Square &to, Board *curr_bord, Board *next_board, const PieceColour &col);
-    std::string toChessNotation();
+    std::string toAlgebraicNotation();
     
 };
 
--- src/move.cpp
+++ src/move.cpp
@@ -4,12 +4,18 @@
 Move::Move(const Square &from, const Square &to, Board *curr_bord, Board *next_board, const PieceColour &col) : from(from), to(to), curr_board(curr_board), next_board(next_board), col(col) {
     from_piece = curr_board->get_piece(from);
     to_piece = curr_board->get_piece(to);
+
+    AlgebraicPiece[KING] = "K";
+    AlgebraicPiece[QUEEN] = "Q";
+    AlgebraicPiece[ROOK] = "R";
+    AlgebraicPiece[BISHOP] = "B";
+    AlgebraicPiece[KNIGHT] = "K";
+    AlgebraicPiece[PAWN] = "";
 }
 
-std::string Move::toChessNotation() {
-    bool capture = false;
-    if (to_piece) capture = true;
-
+std::string Move::toAlgebraicNotation() {
     std::string from_nottion, to_notation;
     to_notation = to.chess_notation_pos();
+    if (to_piece) to_notation = "x" + to_notation; // capture
+    from_nottion = AlgebraicPiece[from_piece->get_type()];
 }

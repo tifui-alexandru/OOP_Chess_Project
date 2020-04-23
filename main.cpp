77f14079420c6dfa36391d8d015592e24b23922f
usureluflorianr <38779396+usureluflorianr@users.noreply.github.com>
2020-04-23 21:33:53 +0300

    front

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
+#include <bits/stdc++.h>
 #include <SFML/Graphics.hpp>
-#include <iostream>
+using namespace sf;
 
-int main() {
-    std::cout << "Usu e fraier";
+#include "src\MainMenu.cpp"
+//#include "src/GameMenu.cpp"
+//#include "src/game1v1.cpp"
+
+int main()
+{
+
+//    menuMode t;
+//    t.menuRun();
+
+
+//     menuGame t1;
+//     menuMode t2;
+//     Game1v1 t3;
+//
+//     do{
+//        t1.menuRun();
+//        if(t1.playButton == true) t2.menuRun();
+//     }while(t1.terminate == false && t2.terminate == false && t3.terminate == false);
     return 0;
 }

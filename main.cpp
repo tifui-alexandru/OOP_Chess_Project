97d511cd2b70c1463964c18a147a4bc135b7dec1
tifui-alexandru <itifui00@gmail.com>
2020-04-28 22:48:21 +0300

    my commit

--- main.cpp
+++ main.cpp
@@ -2,115 +2,118 @@
 #include <SFML/Graphics.hpp>
 using namespace sf;
 
-//#include "include/MainMenu.h"
-//#include "include/GameMenu.h"
-//#include "include/game1vs1.h"
-//#include "include/about.h"
+#include "include/MainMenu.h"
+#include "include/GameMenu.h"
+#include "include/game1vs1.h"
+#include "include/game1vsPC.h"
+#include "include/about.h"
 
 int main()
 {
-    sf::RenderWindow window( sf::VideoMode( 400, 600 ), "Awesome Game" );
+    // sf::RenderWindow window( sf::VideoMode( 400, 600 ), "Awesome Game" );
 
-    sf::Time micro = sf::microseconds( 10000 );
-    sf::Time milli = sf::milliseconds( 10 );
-    sf::Time second = sf::seconds( 0.01f );
+    // sf::Time micro = sf::microseconds( 10000 );
+    // sf::Time milli = sf::milliseconds( 10 );
+    // sf::Time second = sf::seconds( 0.01f );
 
-    //std::cout << micro.asSeconds( ) << std::endl;
-    //std::cout << micro.asMilliseconds( ) << std::endl;
-    //std::cout << micro.asMicroseconds( ) << std::endl;
+    // //std::cout << micro.asSeconds( ) << std::endl;
+    // //std::cout << micro.asMilliseconds( ) << std::endl;
+    // //std::cout << micro.asMicroseconds( ) << std::endl;
 
-    //std::cout << ( micro + milli ).asSeconds( ) << std::endl;
+    // //std::cout << ( micro + milli ).asSeconds( ) << std::endl;
 
-    sf::Clock clock; // starts the clock
-    float pwp = 30 * 60;
+    // sf::Clock clock; // starts the clock
+    // float pwp = 30 * 60;
 
-    while ( window.isOpen( ))
-    {
-        sf::Event event;
+    // while ( window.isOpen( ))
+    // {
+    //     sf::Event event;
 
-        while ( window.pollEvent( event ) )
-        {
-            switch ( event.type )
-            {
-                case sf::Event::Closed:
-                    window.close( );
+    //     while ( window.pollEvent( event ) )
+    //     {
+    //         switch ( event.type )
+    //         {
+    //             case sf::Event::Closed:
+    //                 window.close( );
 
-                    break;
+    //                 break;
 
-                case sf::Event::KeyReleased:
-                    clock.restart( );
+    //             case sf::Event::KeyReleased:
+    //                 clock.restart( );
 
-                    break;
-            }
-        }
-        window.clear( );
+    //                 break;
+    //         }
+    //     }
+    //     window.clear( );
 
-        sf::Text text;
+    //     sf::Text text;
 
-        sf::Font font;
-        if(!font.loadFromFile("../images/sans.ttf")){}
+    //     sf::Font font;
+    //     if(!font.loadFromFile("../images/sans.ttf")){}
 
-        sf::Time elapsed = clock.getElapsedTime( );
-        std::cout << elapsed.asSeconds( ) << std::endl;
-        //float a = elapsed.asSeconds();
-        int t1 = elapsed.asSeconds();
+    //     sf::Time elapsed = clock.getElapsedTime( );
+    //     std::cout << elapsed.asSeconds( ) << std::endl;
+    //     //float a = elapsed.asSeconds();
+    //     int t1 = elapsed.asSeconds();
 
-        t1 = pwp - t1;
-        if(t1 < 0) break ;
+    //     t1 = pwp - t1;
+    //     if(t1 < 0) break ;
 
-        text.setFont(font);
+    //     text.setFont(font);
 
-        int t2 = t1 % 60;
+    //     int t2 = t1 % 60;
 
-        t1 /= 60;
+    //     t1 /= 60;
 
-        std::string usu = "";
-        if(t1 < 10) usu += '0';
-        usu += std::to_string(t1);
+    //     std::string usu = "";
+    //     if(t1 < 10) usu += '0';
+    //     usu += std::to_string(t1);
 
-    // menuGame t1;
-    // menuMode t2;
-    // menuAbout t4;
+    // // menuGame t1;
+    // // menuMode t2;
+    // // menuAbout t4;
 
-        //std::string usu2="";
+    //     //std::string usu2="";
 
-        //int k = 0;
-        //while(usu[k] != '.') usu2.push_back(usu[k++]);
+    //     //int k = 0;
+    //     //while(usu[k] != '.') usu2.push_back(usu[k++]);
 
-        //usu2.push_back('.');
-        //++k;
-        //usu2.push_back(usu[k++]);
-        //usu2.push_back(usu[k++]);
+    //     //usu2.push_back('.');
+    //     //++k;
+    //     //usu2.push_back(usu[k++]);
+    //     //usu2.push_back(usu[k++]);
 
-        text.setString(usu);
+    //     text.setString(usu);
 
-        text.setCharacterSize(80);
+    //     text.setCharacterSize(80);
 
-        text.setFillColor(sf::Color::Red);
+    //     text.setFillColor(sf::Color::Red);
 
-        text.setStyle(sf::Text::Bold);
+    //     text.setStyle(sf::Text::Bold);
 
-        text.setPosition(100, 200);
+    //     text.setPosition(100, 200);
 
-        window.draw(text);
+    //     window.draw(text);
 
 
 
-        window.display( );
-    }
+    //     window.display( );
+    // }
 
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-    //menuAbout t;
+    menuAbout t;
     //t.menuRun();
 
-//    menuGame t1;
-//    menuMode t2;
-//    Game1vs1 t3;
-//    menuAbout t4;
+   menuGame t1;
+   menuMode t2;
+   //Game1vs1 t3;
+   Game1vsPC t3;
+   t3.play();
+   menuAbout t4;
 //
 //    bool menuModeGo = false;
 

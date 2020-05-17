33592f40cec3f0c6c148af7f5f2c6106216e84ce
tifui-alexandru <itifui00@gmail.com>
2020-05-17 23:02:57 +0300

    shared_ptr

--- include/gameMode.h
+++ include/gameMode.h
@@ -1,25 +1,27 @@
 #ifndef GAMEMODE_H
 #define GAMEMODE_H
 
+#include <memory>
 #include "game1vsPC.h"
 #include "game1vs1.h"
 
-
 class GameMode {
 private:
-    std::unique_ptr<GameFront> strategy;
-    //GameFront* strategy;
+    std::shared_ptr <GameFront> strategy;
 public:
     enum StrategyType {
         _1VS1, _1VSPC
     };
+
     GameMode() {
         strategy = nullptr;
     }
+
     void setStrategy(StrategyType type) {
-        if (type == _1VS1) strategy = std::make_unique<Game1vs1>();
-        else if (type == _1VSPC) strategy = std::make_unique<Game1vsPC>();
+        if (type == _1VS1) strategy = std::make_shared<Game1vs1>();
+        else if (type == _1VSPC) strategy = std::make_shared<Game1vsPC>();
     }
+
     void play() {
         strategy->play();
     }
